6e33feb52851f347b5dfae346ab0f62be21cb9bc
snk-js <julianoh1909@gmail.com>
2022-03-30 18:26:27 -0300

    add sample test for agg

---
+++ src/__tests__/api/v0/transaction/index.test.tsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+describe('MongoDB API payload requests', () => {
+  it('get chart data of latest CryptoPunks v1 sales over past 7 days', async () => {
+    const chartData = axios.post('http://localhost:3000/api/v0/aggregate', {
+      nft_event: 'sale',
+      nft_contract: '0x282bdd42f4eb70e7a9d9f40c8fea0825b7f68c5d',
+      date: 'gte:2022-03-21',
+      operation: 'sum',
+      group_by: 'day',
+      field: 'value.shifted',
+      num: 7,
+    });
+
+    expect(chartData).toBe({});
+  });
+});
