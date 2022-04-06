c7793828b6bf8ffe5755f9c26df70b237e04a401
snk-js <julianoh1909@gmail.com>
2022-04-05 21:39:54 -0300

    refactor get variables from body route params

--- src/utils/mongoDb/aggregation/genPipe.ts
+++ src/utils/mongoDb/aggregation/genPipe.ts
@@ -10,17 +10,15 @@ import { formatMongoDate } from '../../dateUtils/index';
 export const genPipe = (params: Record<string, any>) => {
   const { filter, aggregate, limit, fields } = params;
 
-  const {
-    ct_addr,
-    ct_func,
-    tx_success,
-    tx_timestamp,
-    nft,
-    nft_event,
-    coin_buy,
-    coin_sale,
-    coin_tx,
-  } = filter;
+  const ct_addr = filter?.ct_addr;
+  const ct_func = filter?.ct_func;
+  const tx_success = filter?.tx_success;
+  const tx_timestamp = filter?.tx_timestamp;
+  const nft = filter?.nft;
+  const nft_event = filter?.nft_event;
+  const coin_buy = filter?.coin_buy;
+  const coin_sale = filter?.coin_sale;
+  const coin_tx = filter?.coin_tx;
 
   let field = '';
   let op = '';
